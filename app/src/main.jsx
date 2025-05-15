318a2fcfdcfae1afb366a29af36d4cac750bd4fb
Sweta <swetasuwal001@gmail.com>
2025-05-15 20:06:59 +0545

    working dice

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Dictaphone from './speech.jsx'
-import Num from './num.jsx'
+import Dice from './diceGame.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-    <Num />
+    <Dice/>
     </>
   </StrictMode>,
 )
